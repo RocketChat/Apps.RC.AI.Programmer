0c0b1c8c693ce8b1f050f7c33d07078c57c9b767
Ryan Zhou <ryanbowz@outlook.com>
2024-08-12 19:16:09 +0800

    remove unuseful github files

--- definition/repositorySubscriptions.ts
+++
@@ -1,9 +0,0 @@
-import { IUser } from "@rocket.chat/apps-engine/definition/users";
-
-//subscriptions which will be saved in the apps local storage
-export interface IRepositorySubscriptions{
-    webhookId : string,
-    repoName : string,
-    user : IUser ,
-    events : Array<string> 
-}
